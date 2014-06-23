687cc39d9ea9039f9d89136b3a1a410f36618a05
srivatsa6065 <srivatsa6065@gmail.com>
2014-06-23 23:28:17 +0530

    Project Created

---
+++ UCTypeRegistration/AppCode/Developer.cs
@@ -0,0 +1,18 @@
+﻿namespace UCTypeRegistration.AppCode
+{
+    public interface IDeveloper
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        decimal Salary { get; set; }
+    }
+
+    public class Developer : IDeveloper
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
---
+++ UCTypeRegistration/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using UCTypeRegistration.AppCode;
+
+namespace UCTypeRegistration
+{
+    class Program
+    {
+        static void Main()
+        {
+            var container = new UnityContainer();
+
+            //Earlier
+            //container.RegisterType<IDeveloper, Developer>();
+            //container.RegisterType<ITester, Tester>();
+
+            //Registering by convention 
+            container.RegisterTypes(
+                AllClasses.FromLoadedAssemblies(),
+                WithMappings.FromMatchingInterface,
+                WithName.Default);
+            
+
+            var developer = container.Resolve<IDeveloper>();
+            developer.Name = "Srivatsa";
+            Console.WriteLine(developer.Name);
+
+            var tester = container.Resolve<ITester>();
+            tester.Name = "Prasad";
+            Console.WriteLine(tester.Name);
+        }
+    }
+}
